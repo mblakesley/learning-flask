1e744260854f80faa605b69a9914ae9c845db0e3
Michael Blakesley <m.e.blakesley@gmail.com>
2020-04-13 18:33:53 -0700

    Add basic endpoints for get/set/getall
    Using in-memory storage, not DB

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from markupsafe import escape
 
 app = Flask(__name__)
+scientist_list = [
+    {
+        'name': 'einstein',
+        'field': 'physics',
+    },
+    {
+        'name': 'darwin',
+        'field': 'biology',
+    }
+]
 
 
-@app.route('/')
-def hello_world():
-    return 'Yo bitch!'
+@app.route('/scientists')
+def get_scientists():
+    return jsonify(scientist_list)
+
+
+@app.route('/scientists', methods=['POST'])
+def post_scientists():
+    new_sci_dict = request.get_json()
+    scientist_list.append(new_sci_dict)
+    return jsonify(new_sci_dict)
+
+
+@app.route('/scientists/<string:scientist_name>')
+def get_scientist(scientist_name):
+    for sci_dict in scientist_list:
+        if sci_dict['name'] == scientist_name:
+            return jsonify(sci_dict)
+    return 'YOU OUTTA LUCK, SON!'
