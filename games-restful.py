c5215840b0ec73d2e11ee48bb1df85e612cb59ae
Michael Blakesley <m.e.blakesley@gmail.com>
2020-04-20 21:56:49 -0700

    Add CRUD endpoints using in-memory storage

--- games-restful.py
+++ games-restful.py
@@ -3,12 +3,12 @@ from flask_restful import Resource, Api
 
 games_list = [
     {
-        "name": "dead-space",
-        "genre": "survival horror",
+        'name': 'dead-space',
+        'genre': 'survival horror',
     },
     {
-        "name": "portal-2",
-        "genre": "puzzle platformer",
+        'name': 'portal-2',
+        'genre': 'puzzle platformer',
     },
 ]
 
@@ -19,19 +19,48 @@ class Games(Resource):
 
     def post(self):
         new_game = request.get_json()
-        for game in games_list:
-            if game["name"] == new_game["name"]:
-                return "resource already exists. I'm on to you!"
+        if get_game(new_game['name']):
+            return 'resource already exists. I\'m on to you!', 400
         games_list.append(new_game)
         return new_game, 201
 
 
 class Game(Resource):
     def get(self, name):
-        for game in games_list:
-            if game["name"] == name:
-                return game
-        return "resource not found. you gonna cry about it?", 404
+        # I don't like that every method here repeats the pattern of calling get_game, then checking result
+        # but I don't know how to simplify it, since even if I package the 404, these methods still must check result
+        # django seems to get out of this via `raise`, so the runner must be doing try/catch. Doesn't help me here :-/
+        game = get_game(name)
+        if game:
+            return jsonify(game)
+        return 'resource not found. you gonna cry about it?', 404
+
+    def put(self, name):
+        updated_game = request.get_json()
+        if name != updated_game['name']:
+            return 'mismatched names. whaddya tryin\' to pull, anyway?', 400
+        game = get_game(name)
+        if game:
+            game.update(updated_game)
+            return jsonify(game)
+        return 'resource not found. now scram!', 404
+
+    def delete(self, name):
+        game = get_game(name)
+        if game:
+            games_list.remove(game)
+            return 'we deleted it so good', 200
+        return 'resource not found. also you\'re ugly.', 404
+
+
+def get_game(name):
+    for game in games_list:
+        if game['name'] == name:
+            return game
+    ### I think this works, but why on earth would you do this??
+    # def has_name(game):
+    #     return game['name'] == name
+    # return list(filter(has_name, games_list))
 
 
 app = Flask(__name__)
